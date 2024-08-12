39e8b926a67aa21528201ce45730eac8a4db1bf9
Krumil <salettisimone10@gmail.com>
2024-08-12 12:13:19 +0200

    push

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import addresses, update, ens
 
 def create_app() -> FastAPI:
     app = FastAPI()
+    
+    # Enable CORS
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Allows all origins
+        allow_credentials=True,
+        allow_methods=["*"],  # Allows all methods
+        allow_headers=["*"],  # Allows all headers
+    )
+    
     app.include_router(addresses.router)
     app.include_router(update.router)
     app.include_router(ens.router)
