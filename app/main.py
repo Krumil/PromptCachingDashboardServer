21bd0d1c988c204902ba46e9024e0e8959c55862
Krumil <salettisimone10@gmail.com>
2024-12-04 10:16:41 +0100

    push

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from . import app
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://prime-maxi.com"],  # Add http:// version too if needed
+    allow_origins=["https://prime-maxi.com"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 if __name__ == "__main__":
