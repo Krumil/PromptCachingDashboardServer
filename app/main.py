700d76fbeb54adb3232ebcc455cfbe450ad82f60
Krumil <salettisimone10@gmail.com>
2024-12-13 22:15:05 +0100

    push

--- app/main.py
+++ app/main.py
@@ -5,18 +5,19 @@ from . import app
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost",           # Allow localhost without a port
-        "http://localhost:8000",      # Allow localhost on port 8000
-        "http://localhost:3000",      # Allow localhost on port 3000 (common for React dev servers)
-        "https://prime-maxi.com",     # Example production origin
-        "https://*.app-paragonsdao-com.pages.dev/",  # Wildcard subdomains
-        "https://paragonsdao.com",    # Specific domain
-        "https://*.paragonsdao.com"   # Wildcard subdomains
+        "http://localhost",
+        "http://localhost:8000",
+        "http://localhost:3000",
+        "https://prime-maxi.com",
+        "https://paragonsdao.com",
+        "https://www.paragonsdao.com",
     ],
+    allow_origin_regex=r"https://.*\.app-paragonsdao-com\.pages\.dev",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 if __name__ == "__main__":
     import uvicorn
